b687453e176eb615189f779b6b3e98b8adf6913e
Pedroh1510 <pedroh1510@hotmail.com>
2024-02-10 08:09:21 -0300

    melhoria no delete

--- src/domain/adm/admService.js
+++ src/domain/adm/admService.js
@@ -4,15 +4,19 @@ import logger from '../../utils/logger.js';
 
 export default class AdmService {
 	constructor() {}
-	async deleteFiles() {
-		const path = './downloads';
+	async deleteFiles(path='./downloads') {
 		const maxHourLifeTime = 1;
 		const listFiles = await readdir(path);
 		logger.info(`Total files ${listFiles.length}`);
 		for (const fileName of listFiles) {
+			if(fileName.endsWith('.!qB')) continue
 			const pathFile = `${path}/${fileName}`;
 			const fileInfo = await lstat(pathFile);
-			if (fileInfo.isDirectory()) continue;
+			if (fileInfo.isDirectory()) {
+				await this.deleteFiles(pathFile)
+				await this.removeEmptyDir(pathFile)
+				continue
+			};
 			const now = Date.now();
 			const diff = DateFormatter.diff(now, fileInfo.ctime, 'hour');
 			if (diff > maxHourLifeTime) {
@@ -20,5 +24,12 @@ export default class AdmService {
 				await rm(pathFile, { force: true });
 			}
 		}
+	}
+	async removeEmptyDir(filePath){
+		const fileInfo = await lstat(filePath);
+		if (!fileInfo.isDirectory()) return
+		const listFiles = await readdir(filePath);
+		if(listFiles.length) return
+		await rm(filePath, { force: true,recursive:true });
 	}
 }
